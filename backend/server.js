18b4597af38b8a05b51057c9d06b1c498951b0ad
Azizul Hakim Tareq <azizul.tareq@northsouth.edu>
2023-10-14 19:33:01 +0600

    database connected

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
-import express from 'express'
+import express from "express";
+import dotenv from 'dotenv'
+dotenv.config()
+import connectDB from "./database/db.js";
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-const app = express()
+const app = express();
+connectDB();
 
-app.listen((PORT, () => {
-    console.log(`App is running on port ${PORT}`)
-}))
+app.listen(
+  (PORT,
+  () => {
+    console.log(`App is running on port ${PORT}`);
+  })
+);
