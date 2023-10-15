4301c6863cc328bdf9de30aa037172a5c999ce47
Azizul Hakim Tareq <azizul.tareq@northsouth.edu>
2023-10-15 10:45:15 +0600

    comment edit and delete by authorized user

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -15,5 +15,34 @@ const createComment = handleAsync(async (req, res) => {
   res.status(201).json(comment);
 });
 
+const editComment = handleAsync(async (req, res) => {
+  const { id } = req.params;
+  const { text } = req.body;
 
-export { createComment };
+  const comment = await Comment.findById(id);
+
+  if (!comment) {
+    res.status(404);
+    throw new Error('Comment not found');
+  }
+
+  comment.text = text;
+  const updatedComment = await comment.save();
+  res.json(updatedComment);
+});
+
+const deleteComment = handleAsync(async (req, res) => {
+  const { id } = req.params;
+
+  const comment = await Comment.findById(id);
+
+  if (!comment) {
+    res.status(404);
+    throw new Error('Comment not found');
+  }
+
+  await comment.remove();
+  res.json({ message: 'Comment removed' });
+});
+
+export { createComment, editComment, deleteComment };
--- backend/routes/commentRoutes.js
+++ backend/routes/commentRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { createComment } from "../controllers/commentController.js";
+import {
+  createComment,
+  editComment,
+  deleteComment,
+} from "../controllers/commentController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/create", protect, createComment);
+router.put("/update/:id", protect, editComment);
+router.delete("/delete/:id", protect, deleteComment);
 
 export default router;
