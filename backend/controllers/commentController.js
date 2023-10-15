08003e396dc182a6070dea653bf39f4f9320b14b
Azizul Hakim Tareq <azizul.tareq@northsouth.edu>
2023-10-15 11:10:26 +0600

    bug fixed in delete comment controller and protected routes

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -1,6 +1,6 @@
 // commentController.js
-import handleAsync from '../middleware/handleAsync.js';
-import Comment from '../models/commentModel.js';
+import handleAsync from "../middleware/handleAsync.js";
+import Comment from "../models/commentModel.js";
 
 const createComment = handleAsync(async (req, res) => {
   const { postId, text } = req.body;
@@ -23,7 +23,7 @@ const editComment = handleAsync(async (req, res) => {
 
   if (!comment) {
     res.status(404);
-    throw new Error('Comment not found');
+    throw new Error("Comment not found");
   }
 
   comment.text = text;
@@ -31,18 +31,32 @@ const editComment = handleAsync(async (req, res) => {
   res.json(updatedComment);
 });
 
+const getCommentsByPostId = handleAsync(async (req, res) => {
+  const { postId } = req.params;
+
+  const comments = await Comment.find({ postId }).populate("user", "name");
+
+  res.json(comments);
+});
+
 const deleteComment = handleAsync(async (req, res) => {
   const { id } = req.params;
 
-  const comment = await Comment.findById(id);
+  try {
+    const comment = await Comment.findByIdAndDelete(id);
 
-  if (!comment) {
-    res.status(404);
-    throw new Error('Comment not found');
+    if (!comment) {
+      res.status(404);
+      throw new Error("Comment not found");
+    }
+
+    await Comment.deleteOne({ _id: id });
+    res.json({ message: "Comment removed" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error deleting comment", error: error.message });
   }
-
-  await comment.remove();
-  res.json({ message: 'Comment removed' });
 });
 
-export { createComment, editComment, deleteComment };
+export { createComment, editComment, getCommentsByPostId, deleteComment };
--- backend/routes/commentRoutes.js
+++ backend/routes/commentRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   createComment,
   editComment,
+  getCommentsByPostId,
   deleteComment,
 } from "../controllers/commentController.js";
 import { protect } from "../middleware/authMiddleware.js";
@@ -10,6 +11,7 @@ const router = express.Router();
 
 router.post("/create", protect, createComment);
 router.put("/update/:id", protect, editComment);
+router.get("/post/:postId",protect, getCommentsByPostId);
 router.delete("/delete/:id", protect, deleteComment);
 
 export default router;
